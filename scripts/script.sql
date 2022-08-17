1d0b0554aeb638040f5cffca9174791893d74a13
jmorgan <jamorgan8@gmail.com>
2022-08-16 21:28:30 -0500

    we have a query

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,1 +1,16 @@
-date for push 1
+
+
+SELECT a.name,
+       ROUND(((a.rating + p.rating) / 2) ,2) AS avg_rating,
+       ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price,
+       SUM((CAST(a.review_count AS INT) + p.review_count)) AS total_review_count
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+    ON a.name = p.name
+GROUP BY a.name, avg_rating, avg_price
+HAVING ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) <= 1
+    AND SUM((CAST(a.review_count AS INT) + p.review_count)) > 2000000
+ORDER BY avg_rating DESC
+LIMIT 10;
+
+
