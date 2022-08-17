af072b43a0f8133edecab00e8ab43c5fc13c4ae1
jwhankins177 <jwhankins177@gmail.com>
2022-08-16 19:38:42 -0500

    1st push

---
+++ scripts/scripts.sql
@@ -0,0 +1,12 @@
+-- app_store_apps
+
+-- play_store_apps
+-- "name"	"category"	"rating"	"review_count"	"size"	"install_count"	"type"	"price"	"content_rating"	"genres"
+
+SELECT *
+FROM app_store_apps
+LIMIT 1;
+
+SELECT *
+FROM play_store_apps
+LIMIT 1;
