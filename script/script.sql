01c8c6f7a763416d179b06d43c659e7a06975ec8
Matt Botschner <matt.botschner@gmail.com>
2022-08-18 21:06:19 -0500

    rounding to the nearest .5

--- script/script.sql
+++ script/script.sql
@@ -41,19 +41,20 @@ HAVING ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2) <1
 ORDER BY total_review_count DESC
 LIMIT 15;
 
---added rating and genre
-SELECT a.name, p.genres, 
-    CONCAT(p.content_rating, '/', a.content_rating) AS content_rating,
-    ROUND(AVG((a.rating+p.rating))/2,2) AS AVG_rating,
-    ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2)AS           avg_price,
-    cast(a.review_count as int) + Max(p.review_count) as total_review_count
+--added rating and genre, rounded star average
+SELECT a.name, a.primary_genre, 
+     CONCAT(p.content_rating, '/', a.content_rating) AS content_rating,
+     ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2)AS                avg_price,
+     CASE WHEN ROUND(AVG((a.rating+p.rating))/2,2) >=4.75 THEN '5.0'
+     WHEN ROUND(AVG((a.rating+p.rating))/2,2) >=4.25 THEN '4.5'
+     END AS avg_rating_rounded
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
-GROUP BY a.name, p.genres, p.content_rating, a.content_rating, a.review_count
-HAVING ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2) <1
+GROUP BY a.name, a.primary_genre, p.content_rating, a.content_rating
+HAVING ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2) <=1
     AND ROUND(AVG((a.rating+p.rating))/2,2) >= 4.5
-ORDER BY total_review_count DESC
+ORDER BY avg_rating_rounded DESC
 LIMIT 15;
 
 --just app store
