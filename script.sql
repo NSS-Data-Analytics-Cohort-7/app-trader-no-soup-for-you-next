562b39cfcba9ee53277b8e2b508957dc2ff627fa
aaustin07 <aaustin07@outlook.com>
2022-08-18 20:06:58 -0500

    umm..honeslty I'm not sure what is going on just pushing

---
+++ script.sql
@@ -0,0 +1,17 @@
+-- SELECT *
+-- FROM app_store_apps
+--rows 7197
+
+-- SELECT * 
+-- FROM play_store_apps
+-- rows 10840
+
+--3.Content Rating .--app store
+SELECT 
+name,
+price,
+content_rating,
+review_count
+From app_store_apps
+--Group By 1,2,3
+
