a782bfaadb2418d1249c8f83e0d9ef0004a3e8d4
jmorgan <jamorgan8@gmail.com>
2022-08-20 11:54:35 -0500

    comparing app rating and profitability

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,48 +1,64 @@
 --'master list' TOP 10 based on avg rating round to nearest .5
+WITH first_cost AS(
+                    SELECT a.name,
+                        CASE WHEN ROUND(AVG((a.price + replace(p.price, '$','')::numeric)/2),2)<=1 THEN 10000
+                        WHEN ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2)>1 THEN ROUND(AVG((a.price + replace(p.price, '$',                                                                   '')::numeric)/2),2)*10000
+                        END AS initial_cost
+                    FROM app_store_apps AS a
+                    INNER JOIN play_store_apps AS p
+                    ON a.name = p.name
+                    GROUP BY a.name
+                   )
 
 SELECT
     a.name,
     ROUND(ROUND(AVG(a.rating + p.rating)) / 2, 1) AS avg_rating,
     ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price,
-    UPPER(CONCAT(primary_genre, '/', category)) AS app_category
+    UPPER(CONCAT(primary_genre, '/', category)) AS app_category,
+    UPPER(CONCAT(a.content_rating, '/', p.content_rating)) AS content,
+    (((ROUND(ROUND(AVG(a.rating+p.rating)) / 2, 1)*2)+1) * 48000) - fc.initial_cost AS total_profit
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
     ON a.name = p.name
-GROUP BY a.name, avg_price, app_category
+FULL JOIN first_cost AS fc
+    ON a.name = fc.name
+GROUP BY a.name, avg_price, app_category, content, fc.initial_cost
 HAVING ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) <= 1
-ORDER BY avg_rating DESC;
+ORDER BY avg_rating DESC
+LIMIT 10;
 
 
-/*SELECT *
-   
-    --ROUND(((a.rating + p.rating) / 2) ,2) AS avg_rating,
-    --ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price,
-   -- SUM((CAST(a.review_count AS INT) + p.review_count)) AS total_review_count
-FROM app_store_apps AS a
-INNER JOIN play_store_apps AS p
-    ON a.name = p.name;
-    
- 
-SELECT *
-   
-FROM play_store_apps
-WHERE name LIKE 'ROBLOX';
+--
 
-SELECT
-    DISTINCT primary_genre
-FROM app_store_apps;*/
+WITH first_cost AS(
+                    SELECT a.name,
+                        CASE WHEN ROUND(AVG((a.price + replace(p.price, '$','')::numeric)/2),2)<=1 THEN 10000
+                        WHEN ROUND(AVG((a.price + replace(p.price, '$', '')::numeric)/2),2)>1 THEN ROUND(AVG((a.price + replace(p.price, '$',                                                                   '')::numeric)/2),2)*10000
+                        END AS initial_cost
+                    FROM app_store_apps AS a
+                    INNER JOIN play_store_apps AS p
+                    ON a.name = p.name
+                    GROUP BY a.name
+                   )
 
 SELECT
     a.name,
-    ROUND(AVG(a.rating + p.rating) ,2) AS avg_rating,
-    ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price
-    --primary_genre AS app_category
+    ROUND(ROUND(AVG(a.rating + p.rating)) / 2, 1) AS avg_rating,
+    ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price,
+    UPPER(CONCAT(primary_genre, '/', category)) AS app_category,
+    UPPER(CONCAT(a.content_rating, '/', p.content_rating)) AS content,
+    ROUND((((ROUND(ROUND(AVG(a.rating+p.rating)) / 2, 1)*2)+1) * 48000), 2) - fc.initial_cost AS total_profit
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
     ON a.name = p.name
-GROUP BY a.name, avg_price
-HAVING ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) <= 1
-ORDER BY avg_rating DESC
-LIMIT 12;
+FULL JOIN first_cost AS fc
+    ON a.name = fc.name
+GROUP BY a.name, avg_price, app_category, content, fc.initial_cost
+ORDER BY avg_rating DESC, total_profit DESC;
+
+
+
+
+
 
 
