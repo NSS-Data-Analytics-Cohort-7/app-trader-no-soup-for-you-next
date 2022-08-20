8045f9aa154c11ea951d5789bdc8cb13e1444ba5
aaustin07 <aaustin07@outlook.com>
2022-08-20 10:28:21 -0500

    just working on script

--- script1.sql
+++ script1.sql
@@ -7,17 +7,21 @@
 -- rows 10840
 
 --3.Content Rating .--app store
--- SELECT 
--- name,
--- price,
--- content_rating,
--- review_count,
--- primary_genre
--- From app_store_apps
--- WHERE content_rating BETWEEN '+4' ANd '9+'
--- ORDER BY content_rating DESC
--- LIMIT 10
---Group By 1,2,3
+
+SELECT
+name,
+content_rating,
+review_count,
+primary_genre,
+SUM(Price)
+
+From app_store_apps
+WHERE content_rating BETWEEN '+4' AND '9+'
+AND primary_genre = 'games'
+GROUP BY 1,2,3,4
+ORDER BY content_rating DESC
+LIMIT 10
+
 
 --play_store_apps
 -- SELECT
@@ -32,6 +36,33 @@
 -- WHERE rating >= 4.0
 -- ORDER BY rating DESC
 -- LIMIT 10
+
+
+
+
+
+
+
+
+-- SELECT -- Jake's Query
+--     a.name,
+--     ROUND(ROUND(AVG(a.rating + p.rating)) / 2, 1) AS avg_rating,
+--     ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) AS avg_price,
+--     UPPER(CONCAT(primary_genre, '/', category)) AS app_category
+-- FROM app_store_apps AS a
+-- INNER JOIN play_store_apps AS p
+--     ON a.name = p.name
+-- GROUP BY a.name, avg_price, app_category
+-- HAVING ROUND(((a.price + REPLACE(p.price, '$', '')::NUMERIC) / 2),2) <= 1
+-- ORDER BY avg_rating DESC;
+
+
+
+
+
+
+
+
 
 
 
